1d44b3e8ef0ecac097368db64101d7db212633e3
acivgin <amar.civgin@gmail.com>
2018-05-10 11:46:56 +0200

    Added family relation detection and minor fixes

--- Task2/T2S1.py
+++ Task2/T2S1.py
@@ -1,5 +1,7 @@
 import csv
 import numpy as np
+import operator
+
 
 titles = ['Mr', 'Mrs', 'Miss', 'Ms', 'Master',
           'Madame', 'Mlle',
@@ -11,7 +13,7 @@ col = ['Captain', 'Colonel', 'Col', 'Major']
 mrs = ['Mrs', 'Madame']
 ms = ['Ms', 'Miss', 'Mlle']
 
-older_male = ['Mr', 'Col', 'Dr', 'Rev']
+older_male = ['Mr', 'Col', 'Dr', 'Rev', 'Sir']
 
 titles_final = ['Mr', 'Mrs', 'Ms', 'Master', 'Col', 'Sir', 'Lady', 'Dr', 'Rev']
 
@@ -35,6 +37,10 @@ def repack_csv(csvreader):
             passenger.spouse_name = passenger.name.split(' ')[0]
             probable_spouse = True
             # this is safe because we later ommit the spouse name attribute
+        if passenger.title == 'Lady':
+            spouse_name = [x.name for x in pass_list if x.title == 'Sir' and x.surname == passenger.surname]
+            if spouse_name:
+                passenger.spouse_name = spouse_name[0].split(' ')[0]
 
         if passenger.spouse_name:
             spouse = next((x for x in pass_list if is_spouse(x, passenger)), None)
@@ -46,7 +52,6 @@ def repack_csv(csvreader):
                     passenger.name = ' '.join(passenger.name.split(' ')[2:])
 
     # age estimation based on the median age of a specific passenger title and p_class set
-
     # firstly we remove the badly loaded data and change it with the previous neighbour value
     bad_data = [x for x in pass_list if not '1' <= x.p_class <= '3']
     for bad_elem in bad_data:
@@ -59,8 +64,10 @@ def repack_csv(csvreader):
     for title in titles_final:
         for i in range(1, 3):
             age_list = np.array([x.age for x in pass_list if x.title == title and x.p_class == i and x.age > 0])
+
             for elem in pass_list:
                 if elem.age == -1:
+                    # TODO add stddev
                     elem.age = np.median(age_list)
 
     # title estimation based on age and sex
@@ -85,9 +92,24 @@ def repack_csv(csvreader):
             else:
                 elem.title = 'Mrs' if elem.age > ms_age_threshold else 'Ms'
 
+    # find family relations,if a person is onboard with someone with the same surname and
+    # is in the same passenger class (families always travel together) they're probably related
+    for elem in pass_list:
+        if elem.family_id:
+            continue
+
+        possible_elem_fam_list = [x for x in pass_list if x.surname == elem.surname and x.p_class == elem.p_class]
+        if len(possible_elem_fam_list) > 1:
+            if not elem.p_class == 3 or len(possible_elem_fam_list) < 5:
+                possible_elem_fam_list.sort(key=operator.attrgetter('age'), reverse=True)
+                possible_elem_fam_ids = [x.id for x in possible_elem_fam_list]
+
+                for ids in possible_elem_fam_ids:
+                    pass_list[ids].family_id = possible_elem_fam_ids[0]
+
     # save the extracted data to a new csv file
     with open('Task2/Data/Titanic_dataset_extended.csv', 'w', newline='') as csvfile:
-        fieldnames = ['id', 'surname', 'title', 'name', 'age', 'sex', 'p_class', 'spouse_id', 'survived']
+        fieldnames = ['id', 'surname', 'title', 'name', 'age', 'sex', 'p_class', 'spouse_id', 'family_id', 'survived']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         writer.writeheader()
@@ -137,6 +159,7 @@ class Passenger:
         self.title = 'Ms' if self.title in ms else self.title
 
         self.spouse_id = None
+        self.family_id = None
 
     def __str__(self):
         return '{}, {} {} a:{} s:{} sur:{}'.format(self.title, self.surname, self.name,
