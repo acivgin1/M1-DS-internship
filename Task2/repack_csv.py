56ee2578a8f10a6b82012653c140c7ac80693797
acivgin <amar.civgin@gmail.com>
2018-05-11 12:58:10 +0200

    Added a new file and changed some of the visualizations.

---
+++ Task2/repack_csv.py
@@ -0,0 +1,177 @@
+import csv
+import numpy as np
+import operator
+
+titles = ['Mr', 'Mrs', 'Miss', 'Ms', 'Master',
+          'Madame', 'Mlle',
+          'Captain', 'Colonel', 'Col', 'Major',
+          'Sir', 'Lady',
+          'Dr',
+          'Rev']
+col = ['Captain', 'Colonel', 'Col', 'Major']
+mrs = ['Mrs', 'Madame']
+ms = ['Ms', 'Miss', 'Mlle']
+
+older_male = ['Mr', 'Col', 'Dr', 'Rev', 'Sir']
+
+titles_final = ['Mr', 'Mrs', 'Ms', 'Master', 'Col', 'Sir', 'Lady', 'Dr', 'Rev']
+
+
+def is_spouse(husband, wife):
+    return husband.surname == wife.surname and \
+           husband.name.split(' ')[0] == wife.spouse_name and \
+           husband.title in older_male
+
+
+def repack_csv(csvreader):
+    pass_list = [Passenger(row) for row in csvreader]
+
+    # adding onboard spouse id, spouse name is later ignored since it was
+    # probably irrelevant to the survival probability
+
+    for passenger, ind in zip(pass_list, range(len(pass_list))):
+        passenger.id = ind
+        probable_spouse = False
+        if len(passenger.name.split(' ')) > 3:  # this could be a potentional spouse
+            passenger.spouse_name = passenger.name.split(' ')[0]
+            probable_spouse = True
+            # this is safe because we later ommit the spouse name attribute
+        if passenger.title == 'Lady':
+            spouse_name = [x.name for x in pass_list if x.title == 'Sir' and x.surname == passenger.surname]
+            if spouse_name:
+                passenger.spouse_name = spouse_name[0].split(' ')[0]
+
+        if passenger.spouse_name:
+            spouse = next((x for x in pass_list if is_spouse(x, passenger)), None)
+            if spouse:
+                spouse.spouse_name = passenger.name.split(' ')[0]
+                spouse.spouse_id = pass_list.index(passenger)
+                passenger.spouse_id = pass_list.index(spouse)
+                if probable_spouse:
+                    passenger.name = ' '.join(passenger.name.split(' ')[2:])
+
+    # age estimation based on the median age of a specific passenger title and p_class set
+    # firstly we remove the badly loaded data and change it with the previous neighbour value
+    bad_data = [x for x in pass_list if not '1' <= x.p_class <= '3']
+    for bad_elem in bad_data:
+        bad_elem.p_class = pass_list[bad_elem.id - 1].p_class
+
+    for elem in pass_list:
+        elem.p_class = int(elem.p_class)
+
+    for title in titles_final:
+        for i in range(1, 4):
+            age_list = np.array([x.age for x in pass_list if x.title == title and x.p_class == i and x.age > 0])
+
+            if i == 2 or i == 3:
+                if title == 'Master' or title == 'Mrs' or title == 'Ms':
+                    print('class: {}, title: {}, est_age: {}'.format(i, title, np.median(age_list)))
+
+            for elem in pass_list:
+                if elem.age == -1:
+                    elem.age = round(np.median(age_list) + 0.9 * np.std(age_list) * np.random.randn(1)[0])
+
+    # title estimation based on age and sex
+    median_title_age = []
+    for title in titles_final[0:4]:
+        age_list = np.array([x.age for x in pass_list if x.title == title])
+        median_title_age.append(np.median(age_list))
+
+    #           Mr      Mrs     Ms      Master
+    # mean:     36.35,  38.5,   29.7,   20.9
+    # median:   41,     41,     36,     11
+    # a Master is someone who is male and younger than 3/4 * 11 + 1/4 * 41 = 21
+    # a Miss is someone who is female and younger than 3/4 * 36 + 1/4 * 41 = 37
+
+    ms_age_threshold = (median_title_age[1] + median_title_age[2] * 3) / 4
+    mr_age_threshold = (median_title_age[0] + median_title_age[3] * 3) / 4
+
+    for elem in pass_list:
+        if not elem.title:
+            if elem.sex == 'male':
+                elem.title = 'Mr' if elem.age > mr_age_threshold else 'Master'
+            else:
+                elem.title = 'Mrs' if elem.age > ms_age_threshold else 'Ms'
+
+    # find family relations,if a person is onboard with someone with the same surname and
+    # is in the same passenger class (families always travel together) they're probably related
+    for elem in pass_list:
+        if elem.family_id:
+            continue
+
+        possible_elem_fam_list = [x for x in pass_list if x.surname == elem.surname and x.p_class == elem.p_class]
+        if len(possible_elem_fam_list) > 1:
+            if not elem.p_class == 3 or len(possible_elem_fam_list) < 5:
+                possible_elem_fam_list.sort(key=operator.attrgetter('age'), reverse=True)
+                possible_elem_fam_ids = [x.id for x in possible_elem_fam_list]
+
+                for ids in possible_elem_fam_ids:
+                    pass_list[ids].family_id = possible_elem_fam_ids[0]
+
+    # save the extracted data to a new csv file
+    with open('Data/Titanic_dataset_extended_1.csv', 'w', newline='') as csvfile:
+        fieldnames = ['id', 'surname', 'title', 'name', 'age', 'sex', 'p_class', 'spouse_id', 'family_id', 'survived']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for elem in pass_list:
+            writer.writerow(elem.to_dictionary(fieldnames))
+
+
+class Passenger:
+    def __init__(self, csv_row):
+        self.id = None
+        self.surname = csv_row['Name'].split(',')[0].split('(')[0]
+        # there is the example of surname (surname_alt) with surname_alt being the alternative surname pronounciation
+        # or the maiden name, we ommit it
+
+        if len(csv_row['Name'].split(',')) > 1:
+            rest = csv_row['Name'].split(',')[1][1:]
+        else:
+            self.surname = csv_row['Name'].split(' ')[0]
+            rest = ' '.join(csv_row['Name'].split(' ')[1:])
+
+        if len(rest.split('(')) > 1:
+            self.title = rest.split(' ')[0] if rest.split(' ')[0] in titles else 'Master'
+            if self.title == 'the':
+                self.title = 'Lady'
+
+            self.name = rest.split('(')[1][:-1]
+            self.spouse_name = rest.split(' ')[1].split(' ')[0]  # we just save the first name
+        else:
+            title_name = rest.split(' ')
+            self.title = title_name[0] if title_name[0] in titles else ''
+
+            self.name = ''
+            if len(title_name) > 1:
+                self.name = ' '.join(title_name[1:]) if self.title else ' '.join(title_name)
+
+            self.spouse_name = ''
+
+        self.age = csv_row['Age']
+        self.age = -1 if self.age == 'NA' else float(self.age)
+
+        self.sex = csv_row['Sex']
+        self.survived = csv_row['Survived']
+        self.p_class = csv_row['PClass'][0]
+
+        self.title = 'Col' if self.title in col else self.title
+        self.title = 'Mrs' if self.title in mrs else self.title
+        self.title = 'Ms' if self.title in ms else self.title
+
+        self.spouse_id = None
+        self.family_id = None
+
+    def __str__(self):
+        return '{}, {} {} a:{} s:{} sur:{}'.format(self.title, self.surname, self.name,
+                                                   self.age, self.sex, self.survived)
+
+    def to_dictionary(self, fieldnames):
+        attr_list = [self.__dict__[x] for x in fieldnames]
+        return dict(zip(fieldnames, attr_list))
+
+
+if __name__ == '__main__':
+    with open('Data/Titanic_dataset.csv') as csvfile:
+        titanic_reader = csv.DictReader(csvfile)
+        repack_csv(titanic_reader)
--- Task2/visualisations.py
+++ Task2/visualisations.py
@@ -1,22 +1,14 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-from pandas.plotting import scatter_matrix
+
 
 titles_final = ['Mr', 'Mrs', 'Ms', 'Master', 'Col', 'Sir', 'Lady', 'Dr', 'Rev']
 
-df = pd.read_csv('Data/Titanic_dataset_extended_1.csv', na_values=[100])
+df = pd.read_csv('Data/Titanic_dataset_extended.csv', na_values=[100])
 df['died'] = np.ones((df.shape[0], 1), dtype=np.uint8) - df['survived'].as_matrix().reshape((-1, 1))
+df['ones'] = np.ones((df.shape[0], 1), dtype=np.uint8)
 
-#survive_title = df.groupby(['title']).agg({'survived': ['sum'], 'died': ['sum']})
-
-# df['title'].replace(to_replace=titles_final, value=range(9), inplace=True)
-# df['title'].astype(int)
-
-plt.figure()
-
-# <1, 1-6, 6-12, 12-18, 18-28, 28-48, 48-68, >68
-age_intervals = [0, 1, 6, 12, 18, 22, 27, 32, 37, 42, 52, 62, 72]
+age_intervals = [0, 1, 3, 6, 12, 18, 22, 27, 32, 37, 42, 47, 52, 57, 62, 72]
 df['age_interval'] = pd.cut(df.age, age_intervals)
 
 survive_title = df.groupby(['title']).agg({'survived': ['sum'], 'died': ['sum']})
@@ -24,82 +16,24 @@ survive_sex = df.groupby(['sex']).agg({'survived': ['sum'], 'died': ['sum']})
 survive_class = df.groupby(['p_class']).agg({'survived': ['sum'], 'died': ['sum']})
 survive_age_sex = df.groupby(['age_interval', 'sex']).agg({'survived': ['sum'], 'died': ['sum']})
 survive_age_sex_class = df.groupby(['age_interval', 'sex', 'p_class']).agg({'survived': ['sum'], 'died': ['sum']})
+survive_age_title = df.groupby(['age_interval', 'title']).agg({'survived': ['sum'], 'died': ['sum']})
+survive_age_title_class = df.groupby(['age_interval', 'title', 'p_class']).agg({'survived': ['sum'],
+                                                                                'died': ['sum'],
+                                                                                'ones': ['sum']})
+
+survived = survive_age_title_class['survived']['sum'].as_matrix().reshape((-1, 1))
+died = survive_age_title_class['died']['sum'].as_matrix().reshape((-1, 1))
+total = survive_age_title_class['ones']['sum'].as_matrix().reshape((-1, 1))
+
+accuracy = np.maximum(survived, died).sum() / total.sum()
+print(accuracy)
+
+survive_age_title_class.to_csv('Data/age_title_class.csv')
 
 print(survive_title)
 print(survive_sex)
 print(survive_class)
 print(survive_age_sex)
 print(survive_age_sex_class)
-
-# plt.figure()
-# df[['age']].boxplot()
-# plt.show()
-#
-# df.groupby(['p_class']).hist()
-# plt.show()
-#
-# plt.figure()
-# scatter_matrix(df, alpha=0.2, figsize=(6, 6), diagonal='kde')
-# plt.show()
-
-# df_survived = df.loc[df.loc[:, 'survived'] == 1, :]
-# df_dead = pd.DataFrame(df.loc[df.loc[:, 'survived'] == 0, :])
-# df_dead['survived'].replace(to_replace=0, value=1, inplace=True)
-#
-# table_survivors = pd.pivot_table(df_survived, values='survived', index=['title'], columns=['age'], aggfunc=np.sum)
-# table_dead = pd.pivot_table(df_dead, values='survived', index=['title'], columns=['age'], aggfunc=np.sum)
-#
-# table_survivors.fillna(0, inplace=True)
-# table_dead.fillna(0, inplace=True)
-#
-# plot_confusion_matrix(table_survivors.as_matrix().astype(np.uint16),
-#                       xticks=table_survivors.columns.values,
-#                       yticks=table_survivors.index.values,
-#                       normalize=True)
-# plt.figure()
-# plot_confusion_matrix(table_dead.as_matrix().astype(np.uint16),
-#                       xticks=table_dead.columns.values,
-#                       yticks=table_dead.index.values,
-#                       normalize=True)
-#
-# plt.show()
-#
-# plt.figure()
-# plot_confusion_matrix(table_dead.as_matrix().astype(np.uint16),
-#                       xticks=table_dead.columns.values,
-#                       yticks=table_dead.index.values,
-#                       normalize=False)
-# plt.show()
-#
-#
-# def plot_confusion_matrix(cm,
-#                           xticks,
-#                           yticks,
-#                           normalize=False,
-#                           title='Confusion matrix',
-#                           cmap=plt.cm.Blues):
-#     """
-#     This function prints and plots the confusion matrix.
-#     Normalization can be applied by setting `normalize=True`.
-#     """
-#     if normalize:
-#         cm = cm.astype('float')
-#
-#         # cm = cm / cm.sum(axis=1)[:, np.newaxis]
-#         cm = cm / cm.sum(axis=0)[np.newaxis, :]
-#
-#         print("Normalized confusion matrix")
-#     else:
-#         print('Confusion matrix, without normalization')
-#
-#     plt.imshow(cm, interpolation='nearest', cmap=cmap)
-#     plt.title(title)
-#     plt.colorbar()
-#     xtick_marks = np.arange(len(xticks))
-#     ytick_marks = np.arange(len(yticks))
-#     plt.xticks(xtick_marks, xticks, rotation=45)
-#     plt.yticks(ytick_marks, yticks)
-#
-#     plt.tight_layout()
-#     plt.ylabel('True label')
-#     plt.xlabel('Predicted label')
+print(survive_age_title)
+print(survive_age_title_class)
